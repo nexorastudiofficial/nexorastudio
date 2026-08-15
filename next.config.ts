b095a190b24e7a7c16ffebdf91be03614e062488
NexoraStudio <nexorastudiofficial@gmail.com>
2026-08-15 11:14:53 +0100

    GitHub-Pages-setup

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: "export",
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
     ],
   },
+  reactCompiler: true,
 };
 
 export default nextConfig;
+
